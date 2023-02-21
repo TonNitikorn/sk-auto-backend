8d9faab3898c41e4aeb4b52c29b06338bea9365b
Patarapol <84073137+Patarapolarmy@users.noreply.github.com>
2023-02-21 12:56:09 +0700

    Update rate_limiter.js

--- src/middleware/rate_limiter.js
+++ src/middleware/rate_limiter.js
@@ -2,7 +2,7 @@
 const rateLimit = require("express-rate-limit");
 const limiter = rateLimit({
     windowMs: 5 * 60 * 1000, // 5 minutes
-    max: 1, // limit each IP to 5 requests per windowMs
+    max: 5, // limit each IP to 5 requests per windowMs
     message: {
         status: 429,
         message: "Too many request from this IP, please try again after 5 minutes",
