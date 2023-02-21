4d906baca48ed284b133f126071d7effb6ce5367
Patarapol <84073137+Patarapolarmy@users.noreply.github.com>
2023-02-21 12:27:27 +0700

    Update controller.js

--- src/v2/auth/controller.js
+++ src/v2/auth/controller.js
@@ -33,10 +33,9 @@ exports.registerOTP = async (req, res, next) => {
 
         const result = await authServices.registerOTP(req.body);
 
-        res.status(200).json({
-            message: 'Register request success',
-            result
-        });
+        res.status(200).json(
+            result,
+        );
 
     } catch (error) {
         next(error);
@@ -49,10 +48,9 @@ exports.registerverifyOTP = async (req, res, next) => {
 
         const result = await authServices.registerVerifyOTP(req.body)
 
-        res.status(200).json({
-            message: 'Register success',
+        res.status(200).json(
             result
-        });
+        );
 
     } catch (error) {
         next(error);
