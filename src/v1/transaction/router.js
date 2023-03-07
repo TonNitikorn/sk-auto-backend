14740a36581eac97455c2efea7c57e73d6482095
jbsoftkingdoms <b.jaturon@softkingdoms.com>
2023-03-07 12:24:56 +0700

    add history

--- src/v1/transaction/router.js
+++ src/v1/transaction/router.js
@@ -6,6 +6,7 @@ const transactionServices = require('./controller');
 const passport = require('../../middleware/passport');
 
 router.post('/withdraw_request', [passport.authorized], transactionServices.createWithdrawRequest);
+router.get('/get_transaction_history', [passport.authorized], transactionServices.getTransactionHistory);
 
 
 
